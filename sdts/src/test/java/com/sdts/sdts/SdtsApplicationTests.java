fd05cfdbed45dad05ef81fa9fe3927c0940d0fed
dharsana3 <dharsanadurai3@gmail.com>
2026-02-12 19:56:29 +0530

    Initial boilerplate setup

---
+++ sdts/src/test/java/com/sdts/sdts/SdtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdts.sdts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
