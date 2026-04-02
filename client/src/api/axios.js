b381cfbce4fc487859c73bf43fcb71b1c9ad6c28
dharsana3 <dharsanadurai3@gmail.com>
2026-04-02 11:35:21 +0530

    chore: production config for Railway deployment

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,8 @@
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
   headers: {
     "Content-Type": "application/json"
   }
