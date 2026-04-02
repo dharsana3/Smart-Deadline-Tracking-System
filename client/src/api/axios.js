ef091649d99dbd926f026669910ec7ba4b2ee820
dharsana3 <dharsanadurai3@gmail.com>
2026-04-02 14:00:28 +0530

    fix: force rebuild with env variable

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
-
 import axios from "axios";
 
+// API base URL from environment variable
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:8080",
   headers: {
