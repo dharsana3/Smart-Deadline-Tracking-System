b3a06b52f19d6539a3a6dd16436abb8e7f31564a
dharsana3 <dharsanadurai3@gmail.com>
2026-04-03 11:55:18 +0530

    fix: add versel URL to CORS and production config

--- sdts-backend/sdts-backend/src/main/java/com/sdts_backend/config/CorsConfig.java
+++ sdts-backend/sdts-backend/src/main/java/com/sdts_backend/config/CorsConfig.java
@@ -7,18 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "http://localhost:3000",
+                            "https://smart-deadline-tracking-system.vercel.app"
+                        )
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
